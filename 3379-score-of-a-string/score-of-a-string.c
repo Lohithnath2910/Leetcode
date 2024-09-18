d9a8e7e634ff2d272802ea918c7118690c2e5347
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-18 16:45:37 +0530

    Time: 5 ms (27.83%) | Memory: 8.3 MB (15.25%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.c
+++ 3379-score-of-a-string/score-of-a-string.c
@@ -4,10 +4,10 @@ int scoreOfString(char* s)
     int s2 = 0;
     int length = strlen(s);
     int final = 0;
-    for(int i = 1;i < length;i++)
+    for(int i = 0;i < length-1;i++)
     {
-        s1 = s[i-1];
-        s2 = s[i];
+        s1 = s[i];
+        s2 = s[i+1];
         final += abs(s1 - s2);
     }
     return final;
