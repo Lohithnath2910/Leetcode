45ee08ac1ee7def165809dbba20f921b13ad7ac5
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-24 22:32:09 +0530

    Time: 16 ms (14.60%) | Memory: 13.6 MB (80.42%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -8,27 +8,30 @@ public:
         int r[len];
         int sum = 0;
         int j = 0;
-        for(int i = 0; i < len;i++)
+        l[0] = 0;
+        for(int i = 1; i < len;i++)
         {
-            j = i;
+            /*j = i;
             sum = 0;
             while(j != 0)
             {
                 sum += nums[j-1];
                 j--; 
-            }
-            l[i] = sum;
+            }*/
+            l[i] = l[i-1] + nums[i-1];
+            cout<<l[i]<<"\n";
         }
-        for(int i = 0; i < len;i++)
+        r[len-1] = 0;
+        for(int i = len-2; i >= 0;i--)
         {
-            j = i+1;
+            /*j = i+1;
             sum = 0;
             while(j != len)
             {
                 sum += nums[j];
                 j++; 
-            }
-            r[i] = sum;
+            }*/
+            r[i] = r[i+1] + nums[i+1];
         }
         for(int i = 0; i < len ;i++)
         {
