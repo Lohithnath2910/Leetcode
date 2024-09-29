4fdfb527bc5aed313411aa8e7000db8c7db05073
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-29 18:48:57 +0530

    Time: 11 ms (93.68%) | Memory: 27.3 MB (15.63%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     void reverseString(vector<char>& s) 
     {
-        reverse(s.begin(),s.end());    
+        //reverse(s.begin(),s.end());
+        for(int i = 0,j = s.size()-1;i < s.size()/2; i++,j--)
+        {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }    
     }
 };
