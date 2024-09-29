f0bfffe37e0ebccd14e7d0e282c90659cb6a5332
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-29 18:33:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (71.25%) - LeetSync

--- 2886-faulty-keyboard/faulty-keyboard.cpp
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -5,13 +5,13 @@ public:
         string ans;
         for(int i = 0;i < s.size();i++)
         {
-            if(s[i] == 'i')
+            if(s[i] != 'i')
             {
-                reverse(ans.begin(),ans.end());
+                ans.push_back(s[i]);
             }
             else
             {
-                ans.push_back(s[i]);
+                reverse(ans.begin(),ans.end());
             }
             
         }
