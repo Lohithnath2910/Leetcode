ee3d0c346efb091c33003cfeff002c72ef037e9c
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-29 23:38:20 +0530

    Time: 181 ms (38.05%) | Memory: 78 MB (20.90%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -3,23 +3,6 @@ public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) 
     {
         vector<int> tot(n+1,0);
-        /*int sumcurr = 0;
-        int track = 1;
-
-        while(track <= n)
-        {
-            sumcurr = 0;
-            for(int k = 0;k < bookings.size();k++)
-            {
-                if(track >= bookings[k][0] && track <= bookings[k][1])
-                {    
-                    sumcurr += bookings[k][2];
-                }
-            }
-            tot.push_back(sumcurr);
-            track++; 
-        }*/
-
         for(auto v: bookings)
         {
             tot[v[0] - 1] += v[2];
