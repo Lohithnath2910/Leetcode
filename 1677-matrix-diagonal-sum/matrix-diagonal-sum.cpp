4c0e21541e9f45de6195db286cf647c3216ca3e9
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-30 00:17:37 +0530

    Time: 7 ms (90.00%) | Memory: 14.2 MB (42.48%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -5,11 +5,11 @@ public:
         int ans = 0;
         for(int i = 0;i < mat.size();i++)
         {
-            ans += mat[i][i];
-            if(i != mat.size() - i - 1)
-            {
-               ans += mat[i][mat.size() - i - 1]; 
-            }
+            ans += mat[i][i] + mat[i][mat.size() - i - 1];
+        }
+        if(mat.size() % 2 != 0)
+        {
+            ans -= mat[mat.size()/2][mat.size()/2]; 
         }
         return ans;
     }
