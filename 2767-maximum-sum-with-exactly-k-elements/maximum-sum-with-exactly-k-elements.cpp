99fd002cbb960fb83c316fc43ddfbf8d1d3baa29
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-09-30 21:07:18 +0530

    Time: 36 ms (25.10%) | Memory: 76 MB (88.93%) - LeetSync

--- 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.cpp
+++ 2767-maximum-sum-with-exactly-k-elements/maximum-sum-with-exactly-k-elements.cpp
@@ -3,11 +3,6 @@ public:
     int maximizeSum(vector<int>& nums, int k) 
     {
         int sum = *max_element(nums.begin(),nums.end());
-        int res = 0;
-        for(int i = 0;i < k;i++)
-        {
-            res += sum + i;
-        }
-        return res;    
+        return ((k*(k-1))/2 + sum*k);    
     }
 };
