67bd64b3788586c9ffca08b00565abe854b07350
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-10-18 23:06:23 +0530

    Time: 7 ms (99.75%) | Memory: 28.6 MB (47.08%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -6,7 +6,6 @@ public:
         int s = 0;
         for(int i = 0;i < (nums.size()/2) + 1;i++)
         {
-            cout<<nums[i]<<"\n";
             if(nums[i] == nums[i+1])
             {
                 s = nums[i];
