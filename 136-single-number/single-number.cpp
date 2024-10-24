970d99c42e47637e22fccfc07f66511c726c15f8
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2024-10-24 22:01:06 +0530

    Time: 12 ms (62.61%) | Memory: 20.5 MB (23.13%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,10 +3,10 @@ public:
     int singleNumber(vector<int>& nums) 
     {
        sort(nums.begin(),nums.end());
-       for(int i = 1;i < nums.size();i+=2)
+       for(int i = 0;i < nums.size()-1;i+=2)
        {
-            if(nums[i-1]!=nums[i])
-            return nums[i-1];
+            if(nums[i]!=nums[i+1])
+            return nums[i];
        }     
        return nums[nums.size()-1];
     }
