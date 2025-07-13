d6974a005e26ac3dc37c7f7db3599ccba3a51e8a
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2025-07-13 23:45:54 +0530

    Time: 10 ms (58.62%) | Memory: 13 MB (62.18%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -5,8 +5,6 @@ class Solution(object):
             
                 flowerbed[i] = 1
                 n -= 1
-                i += 2
-            else:
                 i += 1
         if(n > 0):
             return False
