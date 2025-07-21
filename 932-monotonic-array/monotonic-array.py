44400968e0643c6ba3490715571e0fb149d645f9
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2025-07-21 23:03:02 +0530

    Time: 5516 ms (5.12%) | Memory: 19.7 MB (100.00%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isMonotonic(self, nums):
+        count1 = 0
+        count2 = 0
+        for i in range(1,len(nums)):
+            print(nums[i-1],nums[i])
+            if(nums[i-1] < nums[i]):
+                count1 += 1
+            elif(nums[i-1] > nums[i]):
+                count2 += 1
+            elif(nums[i-1] == nums[i]):
+                continue
+        
+        if(count1 == 0 or count2 == 0):
+            return True
+        return False
