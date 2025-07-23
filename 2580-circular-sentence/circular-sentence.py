aa8684d514bd7ee98585c5f176d048032d4fe149
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2025-07-23 12:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (79.80%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -5,12 +5,9 @@ class Solution(object):
         strs = sentence.split(" ")
         if(strs[-1][-1] == strs[0][0]):
             e = True
-            print(strs)
             if(len(strs) == 1):
                 return True
         for i in range(1,len(strs)):
-            print(strs[i-1][-1])
-            print(strs[i][0])
             if strs[i-1][-1] != strs[i][0]:
                 f = False
                 break    
