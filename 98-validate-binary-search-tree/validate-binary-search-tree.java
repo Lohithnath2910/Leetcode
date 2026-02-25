03a1b500839d2dcb1ceb57e110738923da35e0c1
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2026-02-25 12:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (34.47%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,28 +14,39 @@
  * }
  */
 class Solution {
-    public List<Integer> k = new ArrayList<>();
-    public void m(TreeNode root)
-    {
-        if(root != null)
-        {
-            m(root.left);
-            k.add(root.val);
-            m(root.right);
-        }
-    }
+    // public List<Integer> k = new ArrayList<>();
+    // public void m(TreeNode root)
+    // {
+    //     if(root != null)
+    //     {
+    //         m(root.left);
+    //         k.add(root.val);
+    //         m(root.right);
+    //     }
+    // }
 
-    public boolean isValidBST(TreeNode root) 
-    {
-        m(root);
-        for(int i = 1;i < k.size();i++)
-        {
-            if(k.get(i-1) >= k.get(i))
-            {
-                return false;
-            }
-        }
+    // public boolean isValidBST(TreeNode root) 
+    // {
+    //     m(root);
+    //     for(int i = 1;i < k.size();i++)
+    //     {
+    //         if(k.get(i-1) >= k.get(i))
+    //         {
+    //             return false;
+    //         }
+    //     }
 
+    //     return true;
+    // }
+    public long mi = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root)
+    {
+        if(root == null) return true;
+
+        if(!isValidBST(root.left)) return false;
+        if(mi >= root.val) return false;
+        mi = root.val;
+        if(!isValidBST(root.right)) return false;
         return true;
     }
 }
