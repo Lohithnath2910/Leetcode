3783d50175b87abacff512c4abee57fdf760a181
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2026-06-17 22:44:36 +0530

    Time: 3 ms (88.44%) | Memory: 48.4 MB (24.74%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,30 +3,7 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int i = 0, j = n-1;
-
-        // if(m == 1)
-        // {
-        //     for(int f = 0;f < n;f++)
-        //     {
-        //         if(matrix[0][f] == target)
-        //         {
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // }
-        // else if(n == 1)
-        // {
-        //     for(int f = 0;f < m;f++)
-        //     {
-        //         if(matrix[f][0] == target)
-        //         {
-        //             return true;
-        //         }
-        //     }
-        //     return false;
-        // }
-
+        
         while(i < m && j >= 0)
         {
             if(matrix[i][j] == target) return true;
