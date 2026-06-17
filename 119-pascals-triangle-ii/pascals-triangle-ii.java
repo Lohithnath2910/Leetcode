df57c9f8a591f5ecb26da9cad3b4c10bf711314d
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2026-06-17 22:30:31 +0530

    Time: 3 ms (2.36%) | Memory: 42.7 MB (17.73%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,28 +1,36 @@
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        if (rowIndex == 0) {
+    public List<Integer> getRow(int numRows) {
+        if(numRows == 0)
+        {
             return new ArrayList<>(Arrays.asList(1));
-        } else if (rowIndex == 1) {
-            return new ArrayList<>(Arrays.asList(1, 1));
+        }
+        else if(numRows == 1)
+        {
+            return new ArrayList<>(Arrays.asList(1,1));
         }
 
         List<List<Integer>> l = new ArrayList<>();
 
-        for (int i = 0; i <= rowIndex; i++) {
+        for(int i = 0;i <= numRows;i++)
+        {
+            int temp = 0;
             l.add(new ArrayList<>());
-
-            for (int j = 0; j <= i; j++) {
+            while(temp <= i)
+            {
                 l.get(i).add(1);
+                temp++;
             }
         }
 
-        for (int i = 2; i <= rowIndex; i++) {
-            for (int j = 1; j < i; j++) {
-                l.get(i).set(j,
-                    l.get(i - 1).get(j - 1) + l.get(i - 1).get(j));
+        for(int i = 2;i <= numRows;i++)
+        {
+            for(int j = 1;j < i;j++)
+            {
+                l.get(i).set(j,l.get(i-1).get(j) + l.get(i-1).get(j-1));
             }
+
         }
 
-        return l.get(rowIndex);
+        return l.get(numRows);
     }
 }
