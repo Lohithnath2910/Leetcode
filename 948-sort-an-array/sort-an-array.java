caf6f5ed36c40299fb0b289d7503f35f262bbd97
Yaganti Lohithnath Reddy <lohithnath2910@gmail.com>
2026-06-19 11:48:09 +0530

    Time: 29 ms (32.45%) | Memory: 82.8 MB (74.96%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,12 +1,60 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Thread sortThread = new Thread(()-> Arrays.sort(nums));
-        sortThread.start();
-        try{
-            sortThread.join();
-        }catch(Exception e){
-
-        }
+        ms(nums,0,nums.length-1);
         return nums;
+    }
+    public void ms(int[] nums,int s,int e)
+    {
+        if(s < e)
+        {
+            int mid = s + (e - s)/2;
+
+            ms(nums,s,mid);
+            ms(nums,mid+1,e);
+            m(nums,s,mid,e);
+        }
+    }
+    public void m(int[] nums,int s,int m,int e)
+    {
+        int k = m - s + 1;
+        int f = e - m;
+
+        int[] n1 = new int[k];
+        int[] n2 = new int[f];
+
+        for(int i = 0;i < k;i++)
+        {
+            n1[i] = nums[s+i];
+        }
+        for(int i = 0;i < f;i++)
+        {
+            n2[i] = nums[m+i+1];
+        }
+
+        int i = 0;
+        int j = 0;
+        int l = s;
+
+        while(i < k && j < f)
+        {
+            if(n1[i] <= n2[j])
+            {
+                nums[l++] = n1[i++];
+            }
+            else
+            {
+                nums[l++] = n2[j++];
+            }
+        }
+
+        while(i < k)
+        {
+            nums[l++] = n1[i++];
+        }
+
+        while(j < f)
+        {
+            nums[l++] = n2[j++];
+        }
     }
 }
